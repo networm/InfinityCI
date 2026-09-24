9148ce9843c5bc323825fe5d0cde1711515c526d
狂飙 <networm@live.com>
2026-09-24 08:02:04 +0800

    chore(website): 部署目标改为 infinityci.github.io 主站根路径（baseUrl /）

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -7,8 +7,8 @@ const config: Config = {
   tagline: '类 Jenkins / GitHub Actions 的自托管持续集成服务器',
   favicon: 'img/favicon.ico',
 
-  url: 'https://networm.github.io',
-  baseUrl: '/InfinityCI/',
+  url: 'https://infinityci.github.io',
+  baseUrl: '/',
 
   organizationName: 'networm',
   projectName: 'InfinityCI',
